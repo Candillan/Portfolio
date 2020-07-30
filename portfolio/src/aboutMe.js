641deb51881ad607fa5187d9235809c1e2619f16
Candillan <calvin.baker128@gmail.com>
2020-07-30 17:14:55 -0400

    getting back into the swing of things

---
+++ portfolio/src/aboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe(){
+    return(
+        <div>
+            <p>Test.</p>
+            {console.log("Hello")}
+        </div>
+    )
+}
+
+export default AboutMe;
