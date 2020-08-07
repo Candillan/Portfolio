fd041cf40e6b97f7eb0cd1deda02ea0f5844277e
Candillan <calvin.baker128@gmail.com>
2020-08-06 22:09:24 -0400

    Update aboutMe.js

--- portfolio/src/aboutMe.js
+++ portfolio/src/aboutMe.js
@@ -4,6 +4,8 @@ function AboutMe(){
     return(
         <div>
             <p>Test.</p>
+            <div>Hello, My name is Calvin Baker. I am a Software Developer based in NYC that is skilled in C++, Javascript, and Ruby. I'm currently practicing to become more proficient in Python at the moment.</div>
+            <div>I have projects linked here that exhibit my strengths as a programmer. The ones listed are a combination of Fullstack Applications that are widely applicable to a professional workspace, as well as personal projects that I do in my spare time. I'm currently seeking a job as a Software Engineer.</div>
             {console.log("Hello")}
         </div>
     )
