c2debfa323c2be3342d1d24dbe5e38e8ea4902ae
Candillan <calvin.baker128@gmail.com>
2020-08-13 22:01:54 -0400

    converted mainpage from function to class

--- portfolio/src/mainPage.js
+++ portfolio/src/mainPage.js
@@ -3,15 +3,17 @@ import AboutMe from './aboutMe.js';
 import Projects from './projects.js';
 import Menu from './menu.js';
 
-function MainPage(){
-    return(
-        <div>
-            {console.log("Make buttons that switch which data is shown from the modules")}
-            <Menu/>
-            <AboutMe/>
-            <Projects/>
-        </div>
-    )
+class MainPage extends React.Component {
+    render(){
+        return(
+            <div>
+                {console.log("Make buttons that switch which data is shown from the modules")}
+                <Menu/>
+                <AboutMe/>
+                <Projects/>
+            </div>
+        )
+    }  
 }
 
 export default MainPage;
