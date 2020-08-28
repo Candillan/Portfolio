76be5d133273cabd031f84f2eb706a9b54b8d610
Candillan <calvin.baker128@gmail.com>
2020-08-27 20:57:21 -0400

    Update projects.js

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Projects(){
     return(
         <div>
-            <div>[Bestie] (bestie image)</div>
-            <div>[Mall Madness] (mall madness image)</div>
+            <div><a href="https://github.com/annacarey/bestie-app-frontend">[Bestie]</a> (bestie image)</div>
+            <div><a href="https://github.com/merewelch214/mall-madness-client">[Mall Madness]</a>(mall madness image)</div>
             <div><a href="https://github.com/Candillan/bathroom-reads-frontend">[Bathroom Reads]</a>(bathroom reads image)</div>
             {console.log("Hello from projects")}
         </div>
